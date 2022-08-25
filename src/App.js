946969a1816767982d5d3e372e6451800e894c98
Trinath Gundla <84904193+GundlaTrinath@users.noreply.github.com>
2022-08-25 22:05:01 +0530

    Login Page created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 import Signup from './Pages/Signup';
+import Login from './Pages/Login';
 
 function App() {
   return (
-    <Signup/>
+    <div>
+      <Signup />
+      <Login />
+    </div>
   );
 }
 
