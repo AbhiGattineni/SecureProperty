3a654e98d48bef77da04ad899fb8f4f72d2d17d9
AbhiGattineni <abhishekgattineni@gmail.com>
2022-08-31 18:24:23 -0700

    corrections2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@
 import './App.css';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
-import Dashboard from './Pages/Dashboard';
 import Nav from './Pages/Nav';
-import Navbar from './Pages/Navbar';
 
 function App() {
   return (
     <div>
-      {/* <Signup />
-      <Login /> */}
+      <Signup />
+      <Login />
       <Nav />
     </div>
   );
