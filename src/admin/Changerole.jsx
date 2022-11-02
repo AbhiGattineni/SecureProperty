3e48769e09842c10bb0fcb892989cb4041abbdef
Trinath Gundla <84904193+GundlaTrinath@users.noreply.github.com>
2022-11-02 21:25:01 +0530

    Edit option added

--- src/admin/Changerole.jsx
+++ src/admin/Changerole.jsx
@@ -7,17 +7,22 @@ function Changerole() {
     const [users, setUsers] = useState([]);
     const [userId, setUserId] = useState('')
     const [changeUser, setChangeUser] = useState('');
+    const [button, setButton] = useState(true);
 
     const handleChangeUsers = () => {
-        const updateDocRef = doc(db, 'users' ,userId);
-        updateDoc(updateDocRef, { role : changeUser})
-        .then(response => {
-            alert("Updated")
-        })
-        .catch(error => {
-            alert("error")
-        })
+        const updateDocRef = doc(db, 'users', userId);
+        updateDoc(updateDocRef, { role: changeUser })
+            .then(response => {
+                alert("Updated")
+                setButton(true)
+            })
+            .catch(error => {
+                alert("error")
+            })
     }
+    const editClick = () => {
+        button === true ? setButton(false) : setButton(true);
+    };
     useEffect(() => {
         const getUsers = async () => {
             const data = await getDocs(usersDbRef);
@@ -63,6 +68,7 @@ function Changerole() {
                                             scope="col"
                                             className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
                                         >
+                                            Edit
                                         </th>
                                     </tr>
                                 </thead>
@@ -79,13 +85,13 @@ function Changerole() {
                                                 {user.emailAddress}
                                             </td>
                                             <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                                {user.role}
+                                                <select defaultValue={user.role} disabled={button} onChange={e => (setChangeUser(e.target.value), setUserId(user.id))} className="p-2 text-black bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+                                                    <option value='User' selected={true}>User</option>
+                                                    <option value='Admin' selected={false}>Admin</option>
+                                                </select>
                                             </td>
                                             <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                                <select onChange={e => (setChangeUser(e.target.value),setUserId(user.id))} className="p-2 text-black bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
-                                                    <option value='User'>User</option>
-                                                    <option value='Admin'>Admin</option>
-                                                </select>
+                                                <button onClick={editClick} className='bg-gray-200 px-3 p-2 rounded'>Edit</button>
                                             </td>
                                         </tr>
                                     ))}
