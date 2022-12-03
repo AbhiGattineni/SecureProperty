afb355500e551048eace9e5e42242185122255e7
AbhiGattineni <abhishekgattineni@gmail.com>
2022-12-02 20:28:44 -0800

    added main image card details and signup with email and password

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,11 @@
-
 const Button = (props) => {
-    return (
-        <button className={props.styles}>{props.name}</button>
-    )
-}
+  return <button className={props.styles}>{props.name}</button>;
+};
 
-Button.defaultProps ={
-    name : 'Button',
-    styles : 'w-20 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg py-2.5',
-}
+Button.defaultProps = {
+  name: "Button",
+  styles:
+    "w-20 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg py-2.5",
+};
 
 export default Button;
